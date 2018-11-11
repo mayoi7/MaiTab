98e16559a976445120658d128f18a3c98479db39
mayoi7 <3451669860@qq.com>
2018-11-11 12:52:51 +0800

    Add 'ENTER' input bind

--- js/newtab.js
+++ js/newtab.js
@@ -6,4 +6,12 @@ $(function() {
     let val = $('#word').val();
     window.open("https://www.baidu.com/baidu?tn=baidu&word=" + val);
   });
+
+  $('#word').bind('keypress',function(event){
+    /* Act on the event */
+    if(event.keyCode == 13) {
+      let val = $('#word').val();
+      window.open("https://www.baidu.com/baidu?tn=baidu&word=" + val);
+    }
+  });
 });
