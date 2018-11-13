6b1c1369acfe1bb7f8dbe5bcb100a7e0c92bbf5f
mayoi7 <3451669860@qq.com>
2018-11-13 20:41:07 +0800

    Fix encoding errors for special characters

--- js/newtab.js
+++ js/newtab.js
@@ -3,14 +3,14 @@ $(function() {
   $('#search').on('click', function(event) {
     event.preventDefault();
     /* Act on the event */
-    let val = $('#word').val();
+    let val = encodeURIComponent($('#word').val());
     window.open("https://www.baidu.com/baidu?tn=baidu&word=" + val);
   });
 
   $('#word').bind('keypress',function(event){
     /* Act on the event */
     if(event.keyCode == 13) {
-      let val = $('#word').val();
+      let val = encodeURIComponent($('#word').val());
       window.open("https://www.baidu.com/baidu?tn=baidu&word=" + val);
     }
   });
